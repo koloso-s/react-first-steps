cde45e6709af6937cc7eeb1bc5886d973c920226
koloso-s <zarychkrystian@gmail.com>
2025-01-23 12:16:18 +0100

    tests

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,35 +1,28 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./style.css"
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const names = ['James','Nicolas','Mike','Johny'];
+  const myData = {
+    'name':'Krystian',
+    'age':18,
+    'school':'ZST Rzeszów'
+  }
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="container">
+      <div className="users">
+      <h1>Hello Users!</h1>
+          {names.map((name)=> (
+            <h3 key={name}>Hello {name}</h3>
+          ))}
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      <div className="myData">
+        <h1>Information about me</h1>
+        <p>My name: <b>{myData.name}</b></p>
+        <p>My age: <b>{myData.age}</b></p>
+        <p>My school: <b>{myData.school}</b></p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </div>
+)
 }
 
 export default App
